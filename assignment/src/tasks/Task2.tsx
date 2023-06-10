9beb105799efb5a9876aa62b4a94b3a2352939b5
unknown <coderanirudh@gmail.com>
2023-06-10 11:18:22 +0530

    created task2

--- assignment/src/tasks/Task2.tsx
+++ assignment/src/tasks/Task2.tsx
@@ -1,18 +1,33 @@
-// import React, { useState, ChangeEvent } from 'react';
-import { Button} from "@nextui-org/react";
+import { useState } from 'react';
+import { Button } from "@nextui-org/react";
 import { Link } from 'react-router-dom';
-// import Props from './Props';
 
 const Task2 = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
   return (
-       <div className="w-screen h-screen overflow-hidden bg-slate-300">
+    <div className="w-screen h-screen overflow-hidden bg-slate-300">
       <div className="px-8 py-8 flex flex-col justify-center items-center">
         <h1 className="text-5xl text-center mt-10 font-semibold font-mono"> Task 2 </h1>
-        <h2 className="text-center mt-16 font-semibold font-mono text-xl lg:text-3xl "> I had to use hooks to update state value </h2>
-        <h2 className="text-center mt-16 font-semibold font-mono text-xl lg:text-3xl "> Here is a counter that gets updated on key press </h2>
+        <h2 className="text-center mt-10 font-semibold font-mono text-xl lg:text-3xl "> I had to use hooks to update state value </h2>
+        <h2 className="text-center mt-10 font-semibold font-mono text-xl lg:text-3xl "> Here is a counter that gets updated on key press </h2>
+        <h2 className="text-center mt-10 font-semibold font-mono text-xl lg:text-3xl "> State value: {count}, Hook used: UseState Hook </h2>
         <div className="flex mt-12 justify-center items-center px-4">
         </div>
-        <div className="flex mt-16">
+        <div className="flex mt-2 justify-center items-center">
+          <Button className='' rounded onClick={decrementCount} size='sm'> - </Button>
+          <h3 className="text-4xl font-semibold mx-4">{count}</h3>
+          <Button className='' rounded onClick={incrementCount} size='sm'> + </Button>
+        </div>
+        <div className="flex mt-12">
           <Link to="/task1b">
             <Button className='mx-4' rounded> 🡸 Back </Button>
           </Link>
@@ -22,7 +37,7 @@ const Task2 = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Task2
+export default Task2;
